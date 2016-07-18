51f90f089082e4b76e89f1d9b4dd6792bc45ce03
Robert McLeod <robert@autogrow.com>
2016-07-19 01:26:59 +1200

    fixed bugs in ian.rb

--- lib/ian.rb
+++ lib/ian.rb
@@ -1,9 +1,20 @@
+
 require 'ian/version'
+require 'ian/control'
+
 require 'fileutils'
 
 module Ian
 
   class << self
+    
+    def debpath(path)
+      "#{path}/DEBIAN"
+    end
+    
+    def ctrlpath(path)
+      "#{debpath(path)}/control"
+    end
     
     def create(name)
       FileUtils.mkdir(name)
@@ -11,14 +22,14 @@ module Ian
     end
 
     def init(path)
-      Ian::Control.new("#{path}/control").save
+      Ian::Control.new(ctrlpath(path)).save
     
       File.write("#{path}/postinst", "#!/bin/bash\n\n\nexit 0;")
       FileUtils.chmod(0755, "#{path}/postinst")   
     end
     
     def control(path)
-      Ian::Control.new(path)
+      Ian::Control.new(ctrlpath(path))
     end
     
     def build_package(path)
