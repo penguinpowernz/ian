1ec25fee1140fe13ffdea1f49cf086a8915895da
Robert McLeod <robert@autogrow.com>
2016-07-19 02:03:12 +1200

    made sure the setting of control vars worked

--- lib/ian/control.rb
+++ lib/ian/control.rb
@@ -12,19 +12,18 @@ module Ian
       end
     end
   
-    def deps
-      []
-    end
-  
+    # allow setting fields directly
     def []=(field, value)
       raise ArgumentError, "Invalid field: #{field}" unless defaults.keys.include?(field)
       @fields[field] = value
     end
     
+    # allow reading fields directly
     def [](field)
       @fields[field]
     end
-  
+
+    # parse this control file into the fields hash  
     def parse
       text = File.read(@path)
       
@@ -40,9 +39,19 @@ module Ian
         @fields[:depends] = @fields[:depends].split(",").map! {|d| d.strip }
       end
       
-      @fields[:long_description] = text.scan(/^  (.*)$/).flatten
+      @fields[:long_desc] = text.scan(/^  (.*)$/).flatten
+    end
+
+    # update a bunch of fields
+    def update(hash)
+      hash.each do |k, v|
+        if fields.keys.include?(k) and v
+          @fields[k] = v
+        end
+      end
     end
   
+    # output the control file as a string
     def to_s
       lines = []
       
@@ -54,15 +63,16 @@ module Ian
         lines << "Depends: #{@fields[:depends].join(", ")}"
       end
       
-      lines << "Description: #{@fields[:description]}"
+      lines << "Description: #{@fields[:desc]}"
       
-      lines += @fields[:long_description].map do |ld|
+      lines += @fields[:long_desc].map do |ld|
         "  #{ld}"
       end
       
       lines.join("\n")
     end
 
+    # save the control file to disk
     def save
       File.write(@path, to_s)
     end
@@ -90,8 +100,8 @@ module Ian
         arch:             "all",
         version:          "0.0.1",
         depends:          [],
-        description:      "This is a description",
-        long_description: [
+        desc:            "This is a description",
+        long_desc:       [
           "This is a longer description that can take",
           "up multiple lines"
         ]
@@ -110,7 +120,7 @@ module Ian
         maintainer:  "Maintainer",
         homepage:    "Homepage",
         arch:        "Architecture",
-        description: "Description"
+        desc:        "Description"
       }
     end
 
