4f911ec2ce10d2b4c37f1b52e63385be397a81e4
Robert McLeod <robert@autogrow.com>
2016-07-19 00:55:23 +1200

    added central file ian.rb

--- lib/ian.rb
+++ lib/ian.rb
@@ -1,5 +1,30 @@
-require "ian/version"
+require 'ian/version'
+require 'fileutils'
 
 module Ian
-  # Your code goes here...
+
+  class << self
+    
+    def create(name)
+      FileUtils.mkdir(name)
+      init(name)
+    end
+
+    def init(path)
+      Ian::Control.new("#{path}/control").save
+    
+      File.write("#{path}/postinst", "#!/bin/bash\n\n\nexit 0;")
+      FileUtils.chmod(0755, "#{path}/postinst")   
+    end
+    
+    def control(path)
+      Ian::Control.new(path)
+    end
+    
+    def build_package(path)
+    
+    end
+    
+  end
+
 end
